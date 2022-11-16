702dab0932ed4d8e8929f35c60e7474b85593d8a
KushalKris96 <kroy60853@gmail.com>
2022-11-16 10:45:51 +0530

    second commit

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 class Demo {
    public static void main(String[] args) {
+	   String s1 = "KUSHAL";
       System.out.println("Hello git");
+      System.out.println(s1.toLowerCase());
       }
    }
